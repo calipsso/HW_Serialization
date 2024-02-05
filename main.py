37d6edfa99d2e0a10cfd263ede1fe79c8fd2e035
calipsso <kamil.vizy@gmail.com>
2024-02-05 22:06:36 +0100

    update

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ class Capitals():
         return vypis
     def adding_data(self,country):
         mesto = input("vloz mesto ako chces:")
-        self.country_dict[country]  = mesto
+        self.country_dict[country] = mesto
         return self.country_dict
 
     def to_json(self, a):
         return json.dumps(a)
 
-    def to_json_file(self, a):
+    def to_json_file(self, nazov):
+        with open(nazov, "w")as file:
+            file.write(json.dumps(self.country_dict))
+        return nazov
 
 
 
@@ -31,17 +34,20 @@ class Capitals():
 
 capitals = Capitals()
 
-finding_data = capitals.finding_data(input("hladaj:"))
-print(finding_data)
-del_capital = capitals.deleting_data(input("zadaj country pre vymaz:"))
-print(del_capital)
-print("------")
-edit_data = capitals.editing_data(input("zadaj country pre edit:"))
-print(capitals.obsah())
-add_data  = capitals.adding_data(input(": zadaj Country pre pridanie: "))
-print(add_data)
-print("------")
+#finding_data = capitals.finding_data(input("hladaj:"))
+#print(finding_data)
+#del_capital = capitals.deleting_data(input("zadaj country pre vymaz:"))
+#print(del_capital)
+#print("------")
+#edit_data = capitals.editing_data(input("zadaj country pre edit:"))
+#print(capitals.obsah())
+#add_data  = capitals.adding_data(input(": zadaj Country pre pridanie: "))
+#print(add_data)
+#print("------")
 print(capitals.to_json(capitals.country_dict))
+print(capitals.to_json_file("to_file.json"))
+
+
 
 
 
